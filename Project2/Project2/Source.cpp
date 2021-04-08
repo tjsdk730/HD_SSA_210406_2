f86c19ba99029a1b84e9de24f334fe8324b0c593
DESKTOP-RDD88MA\LG <tjsdk2486@gmil.com>
2021-04-08 20:21:07 +0900

    예제2

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main() {
+	int x = 10;
+	int y = x / 2;
+	float  pi = 3.141592;
+	float AREA = pi * ((y) * (y));
+	float circum = pi * 2 * y;
+
+	printf("원의 넓이는 %f 이고, 원의 둘레는 %f 입니다.\n ", AREA, circum);
+	fgetc(stdin);
+
+
+	
+
+	
+}
